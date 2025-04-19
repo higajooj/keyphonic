66289699250b4cfd4948603064a823df27227e12
Felipe Soares <felipe.11.11@hotmail.com>
2025-04-19 12:06:15 -0400

    feat: add hide/unhide password

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,45 @@
-import * as React from "react"
+"use client";
 
-import { cn } from "@/lib/utils"
-
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
+import { cn } from "@/lib/utils";
+import { Eye, EyeClosed } from "lucide-react";
+import { ComponentProps, useState } from "react";
+interface InputProps extends ComponentProps<"input"> {
+  label?: string;
+  name: string;
 }
 
-export { Input }
+function Input({ className, type, label, name, ...props }: InputProps) {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <label className="text-xs mb-1.5" htmlFor={name}>
+        {label}
+      </label>
+      <div className="relative ">
+        <input
+          id={name}
+          type={visible ? "text" : type}
+          data-slot="input"
+          className={cn(
+            "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+            "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+            className
+          )}
+          {...props}
+        />
+        {type === "password" && (
+          <button
+            className="rounded cursor-pointer px-3 h-full absolute top-0 right-0"
+            onClick={() => setVisible((v) => !v)}
+          >
+            {visible ? <Eye size={20} /> : <EyeClosed size={20} />}
+          </button>
+        )}
+      </div>
+    </>
+  );
+}
+
+export { Input };
