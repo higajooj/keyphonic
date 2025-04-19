3c43d72279318af93386a28a43a4272b2639563e
Felipe Soares <felipe.11.11@hotmail.com>
2025-04-19 12:06:38 -0400

    feat: add login page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/admin/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section>
+    <section className="min-h-screen flex flex-col">
       <Header />
       {children}
     </section>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-brand-500 font-semibold text-primary-foreground shadow-xs hover:bg-brand-500/90",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        default: "min-h-9 px-8 py-4 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
