bc302080ef7323320447120d8233ee057f597df2
Felipe Soares <felipe.11.11@hotmail.com>
2025-04-19 15:08:14 -0400

    feat: add authenticad header

---
+++ src/components/admin/profile.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+
+interface ProfileProps {
+  name: string;
+  role: string;
+  imageUrl?: string;
+}
+export const Profile = ({ name, role, imageUrl }: ProfileProps) => {
+  const fallback = name
+    .split(" ")
+    .map((item, i, array) => (i === 0 || i == array.length - 1 ? item[0] : ""))
+    .join("");
+
+  return (
+    <Link
+      href="/admin/profile"
+      className="flex items-center gap-2.5 p-2.5 border rounded-xl"
+    >
+      <Avatar>
+        <AvatarImage src={imageUrl} />
+        <AvatarFallback>{fallback}</AvatarFallback>
+      </Avatar>
+
+      <div className="flex flex-col">
+        <h2 className="font-semibold">{name}</h2>
+        <p className="font-medium text-[10px] text-gray-500">{role}</p>
+      </div>
+    </Link>
+  );
+};
