de8c0d0b8e14df7519dd0e4f31696de04713b8c8
Felipe Soares <felipe.11.11@hotmail.com>
2025-04-19 12:44:55 -0400

    feat: integrate login form

--- src/app/admin/auth/page.tsx
+++ src/app/admin/auth/page.tsx
@@ -1,17 +1,54 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const loginFormSchema = z.object({
+  email: z.string().email("e-mail inválido"),
+  password: z.string().min(1, "* obrigatório"),
+});
+
+type loginFormType = z.infer<typeof loginFormSchema>;
 
 export default function Page() {
+  const { push } = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginFormType>({
+    resolver: zodResolver(loginFormSchema),
+  });
+  const onSubmit = (data: loginFormType) => {
+    console.log(data);
+    push("/admin");
+  };
+
   return (
     <div className="grow flex items-center justify-center">
-      <div className="bg-[#F7F7F7] p-8 flex flex-col rounded-xl w-full m-8 max-w-[495px]">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-[#F7F7F7] p-8 flex flex-col rounded-xl w-full m-8 max-w-[495px]"
+      >
         <h1 className="font-bold text-3xl text-center mb-8">KeyPhonic</h1>
         <div className="space-y-2.5 mb-5">
-          <Input name="email" label="E-mail" />
-          <Input name="password" label="Senha" type="password" />
+          <Input
+            label="E-mail"
+            error={errors.email?.message}
+            {...register("email")}
+          />
+          <Input
+            label="Senha"
+            type="password"
+            error={errors.password?.message}
+            {...register("password")}
+          />
         </div>
-        <Button>Entrar</Button>
-      </div>
+        <Button type="submit">Entrar</Button>
+      </form>
     </div>
   );
 }
