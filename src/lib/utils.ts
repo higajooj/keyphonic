406a0f585ae54fb27eebfde140b1ebd42ba42b60
Felipe Soares <felipe.11.11@hotmail.com>
2025-04-20 13:22:46 -0400

    feat: add statiscts card

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatMoney = (value?: number) => {
+  if (value) {
+    const string = value.toString();
+
+    if (string.replace(/\D/g, "") !== "") {
+      const amount = (
+        parseInt(string.replace(/\D/g, ""), 10) / 100
+      ).toLocaleString("pt-BR", {
+        minimumFractionDigits: 2,
+        style: "currency",
+        currency: "BRL",
+      });
+
+      return value < 0 ? `-${amount}` : amount;
+    }
+  }
+
+  return "R$ 0,00";
+};
