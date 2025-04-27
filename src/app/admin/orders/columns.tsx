03d9a261b7bc0e71e3374d52f7bf53d0e8d04b81
Felipe Soares <felipe.11.11@hotmail.com>
2025-04-27 18:35:15 -0400

    feat: formatting orders table

--- src/app/admin/orders/columns.tsx
+++ src/app/admin/orders/columns.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Status, { StatusEnum } from "@/components/admin/Status";
+import { formatMoney } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
-
+import { format } from "date-fns";
 
 export type Order = {
   id: string;
@@ -15,26 +17,44 @@ export type Order = {
 export const columns: ColumnDef<Order>[] = [
   {
     accessorKey: "id",
-    header: "#Pedido",
+    header: () => <div className="text-center">#Pedido</div>,
+    cell: (props) => (
+      <div className="text-center">{props.getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "client",
-    header: "Cliente",
+    header: () => <div className="text-center">Cliente</div>,
+    cell: (props) => (
+      <div className="text-center">{props.getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "total",
-    header: "Total",
+    header: () => <div className="text-center">Total</div>,
+    cell: (props) => (
+      <div className="text-center">{formatMoney(props.getValue<number>())}</div>
+    ),
   },
   {
     accessorKey: "qty",
-    header: "qtd. Items",
+    header: () => <div className="text-center">qtd. Items</div>,
+    cell: (props) => (
+      <div className="text-center">{props.getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "status",
-    header: "Status",
+    header: () => <div className="text-center">Status</div>,
+    cell: (props) => <Status status={props.getValue<StatusEnum>()} />,
   },
   {
     accessorKey: "date",
-    header: "Data",
+    header: () => <div className="text-center">Data</div>,
+    cell: (props) => (
+      <div className="text-center">
+        {format(props.getValue<string>(), "dd/MM/yyyy, HH:mm")}
+      </div>
+    ),
   },
 ];
