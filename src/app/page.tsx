003f4951f3df6fd0a2458554c798469e1629a4bf
Daniel Higa <danielhiga10@gmail.com>
2025-04-27 01:20:14 -0400

    Add early product categories dropdown menus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,37 @@
+import { CircleUser, MapPin } from "lucide-react";
+import Link from "next/link";
+import ProductCategories from "@/components/home/product-categories";
+import { Input } from "@/components/ui/input";
+
 const Home = () => {
   return (
-    <>
-      <div className="flex justify-end bg-black px-4 py-2 text-gray-200 text-xs">
-        {/*TODO: add "pin" icon*/}
-        <span>Deliver to 123456</span>
+    <div className="flex flex-col">
+      <div className="flex items-center justify-end gap-x-2 bg-black px-4 py-2 text-gray-200 text-xs">
+        <MapPin />
+        <span>
+          Deliver to <strong>123456</strong>
+        </span>
       </div>
+
       <div className="flex items-baseline justify-between border-gray-200 border-b px-12 py-6">
         <h1 className="font-bold text-2xl">KeyPhonic</h1>
 
+        <div>
+          <Input name="search" variant="search" />
+        </div>
+
         <div className="asd">
-          <a className="asd" href="#">
-            Sign in
-          </a>
+          <Link className="flex items-center gap-x-2" href="/">
+            <CircleUser size={16} />
+            <span>Sign up</span>
+          </Link>
         </div>
       </div>
-    </>
+
+      <div className="flex justify-center border-b py-6">
+        <ProductCategories />
+      </div>
+    </div>
   );
 };
 
