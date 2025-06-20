2ec26128f1f57a26783a1318612e6c7a2e109515
Felipe Soares <felipe.11.11@hotmail.com>
2025-06-20 18:50:31 -0400

    feat: add product routes

---
+++ backend/prisma/migrations/20250620223839_project_soft_delete/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `code_password` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `is_active` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `users` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "products" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "code_password",
+DROP COLUMN "created_at",
+DROP COLUMN "is_active",
+DROP COLUMN "updated_at",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
---
+++ backend/src/domains/product/use-case/get-all.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { CategoryEnum } from 'generated/prisma';
+import { IProductRepository } from '../interfaces/product.interface';
+import {
+  BasePaginationInputDTO,
+  BasePaginationOutputDTO,
+} from 'src/shared/dto/pagination.dto';
+import { IProduct } from '../product.domain';
+import { makePagination } from 'src/shared/utils/make-pagination';
+
+export class GetAllServiceInput extends BasePaginationInputDTO {
+  filter?: {
+    search?: string;
+    category?: CategoryEnum;
+  };
+}
+
+export class GetAllServiceOutput extends BasePaginationOutputDTO {
+  data: IProduct[];
+}
+
+@Injectable()
+export class GetAllService {
+  constructor(private readonly productRepository: IProductRepository) {}
+
+  public async execute({
+    filter,
+    pagination,
+  }: GetAllServiceInput): Promise<GetAllServiceOutput> {
+    const where: any = { isActive: true };
+
+    if (filter?.category) where.category = filter.category;
+    if (filter?.search) {
+      where.OR = [
+        { name: { contains: filter.search, mode: 'insensitive' } },
+        { description: { contains: filter.search, mode: 'insensitive' } },
+      ];
+    }
+
+    const products = await this.productRepository.findAll({
+      where,
+      ...makePagination(pagination),
+    });
+
+    const total = await this.productRepository.count({ where });
+
+    return {
+      data: products,
+      pagination: {
+        ...pagination,
+        total,
+      },
+    };
+  }
+}
