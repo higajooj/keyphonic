e7d948de13740aac14fcb3eec0781887ded6a900
Daniel Higa <danielhiga10@gmail.com>
2025-06-30 15:40:06 -0400

    Add product to Cart

--- frontend/src/app/(public)/products/[id]/page.tsx
+++ frontend/src/app/(public)/products/[id]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useLocalStorage } from "@uidotdev/usehooks";
 import Image from "next/image";
 import { useParams } from "next/navigation";
+import { MouseEventHandler, useEffect } from "react";
 import ProductImagesCarousel from "@/components/products/product-images-carousel";
 import { Button } from "@/components/ui/button";
 import { useProduct } from "@/hooks/useProduct";
@@ -10,6 +12,17 @@ const ProductPage = () => {
   const params = useParams();
   const productId = (params.id || "") as string;
   const { product } = useProduct(productId);
+  const [cart, setCart] = useLocalStorage("cart", {});
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  const addToCart: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    setCart((prev) => ({ ...prev, [productId]: 1 }));
+    console.log("added");
+  };
 
   return (
     <div className="mt-4 flex justify-center gap-x-8">
@@ -30,7 +43,7 @@ const ProductPage = () => {
 
         <p>{product?.description}</p>
 
-        <Button className="self-end font-semibold text-xs" size="sm" variant="outline">
+        <Button className="self-end font-semibold text-xs" onClick={(e) => addToCart(e)} size="sm" variant="outline">
           Add to cart
         </Button>
       </div>
